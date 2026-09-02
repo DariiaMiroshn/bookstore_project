d86db35a87b3489765054e0214d80ba9dbc74405
Dariia Miroshnychenko <dariyamiroshnichenko@gmail.com>
2026-09-02 13:57:04 -0500

    Add Class-Based Views, templates with Bootstrap, pagination and namespace URLs

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'store'  # Це і є namespace
+
+urlpatterns = [
+    path('', views.BookListView.as_filename if False else views.BookListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('book/add/', views.BookCreateView.as_view(), name='book_create'),
+    path('book/<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
 
-# Create your views here.
+# 1. Список книг з пагінацією
+class BookListView(ListView):
+    model = Book
+    template_name = 'store/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 4  # кількість книг на сторінку
+
+# 2. Деталі книги
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'store/book_detail.html'
+    context_object_name = 'book'
+
+# 3. Створення книги
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'store/book_form.html'
+    fields = ['title', 'author', 'category', 'price', 'description', 'stock']
+    success_url = reverse_lazy('store:book_list')
+
+# 4. Редагування книги
+class BookUpdateView(UpdateView):
+    model = Book
+    template_name = 'store/book_form.html'
+    fields = ['title', 'author', 'category', 'price', 'description', 'stock']
+    success_url = reverse_lazy('store:book_list')
+
+# 5. Видалення книги
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'store/book_confirm_delete.html'
+    success_url = reverse_lazy('store:book_list')
